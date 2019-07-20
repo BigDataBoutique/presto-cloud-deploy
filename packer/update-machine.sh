e49bc79a0d3355119cfd3b33698ecee9aca9d08f
Tigran <tigran@bigdataboutique.com>
2019-07-20 18:42:05 +0400

    Fix packer non-interactive issue

--- packer/update-machine.sh
+++ packer/update-machine.sh
@@ -5,19 +5,19 @@ log() {
   echo "==> $(basename ${0}): ${1}"
 }
 
-DEBIAN_FRONTEND=noninteractive
+export DEBIAN_FRONTEND=noninteractive
 
 TZ=Etc/UTC
 ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
 
 log "Updating package index..."
-sudo apt-get update -qq
+sudo -E apt-get update -qq
 
 log "Upgrading existing packages"
-sudo apt-get upgrade -y -qq
+sudo -E apt-get upgrade -y -qq
 
 log "Installing prerequisites..."
-sudo apt-get install -y -qq --no-install-recommends \
+sudo -E apt-get install -y -qq --no-install-recommends \
    wget software-properties-common htop ntp jq apt-transport-https python
 
 # Disable daily apt unattended updates.
